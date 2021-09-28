89927f2de614b84bb2964a92c8315168d9f87d1e
Bilal Shafi <bshaf001@ucr.edu>
2021-09-28 11:34:43 -0700

    fixed bug with sqlite transactions

--- alphavantage.py
+++ alphavantage.py
@@ -1,8 +1,8 @@
+from datetime import datetime, timedelta, timezone
 from json.decoder import JSONDecodeError
 import time
 import sqlite3
 import requests
-import time
 import csv
 
 alphavantage_key = None
@@ -10,10 +10,20 @@ alphavantage_key = None
 class AlphvantageError(BaseException):
     def __init__(self, msg):
         self.msg = msg
-        pass
+
+def alphavantage_request(uri):
+    time.sleep(1/5)
+    req = requests.get(uri)
+    error = get_alphavantage_error_code(req)
+    if error != None:
+        raise AlphvantageError(error)
+    return req
 
 def alphavantage_db():
-    return sqlite3.connect('databases/alphavantage.db');
+    con = sqlite3.connect('databases/alphavantage.db');
+    # This forces sqlite to not mess up transactions
+    con.isolation_level = None
+    return con
 
 def initialize_alphavantage_db():
     con = alphavantage_db()
@@ -22,7 +32,7 @@ def initialize_alphavantage_db():
     """).fetchall()
     table_names = [table_name for (table_name,) in tables]
     if "candlestick_5min" not in table_names:
-        con.executescript("""
+        con.execute("""
             CREATE TABLE candlestick_5min(
                 symbol TEXT NOT NULL,
                 timestamp INTEGER NOT NULL,
@@ -37,7 +47,7 @@ def initialize_alphavantage_db():
             ON candlestick_5min(symbol, timestamp);
         """)
     if "candlestick_daily" not in table_names:
-        con.executescript("""
+        con.execute("""
             CREATE TABLE candlestick_daily(
                 symbol TEXT NOT NULL,
                 timestamp INTEGER NOT NULL,
@@ -52,13 +62,13 @@ def initialize_alphavantage_db():
             ON candlestick_daily(symbol, timestamp);
         """)
     if "symbols" not in table_names:
-        con.executescript("""
+        con.executet("""
             CREATE TABLE symbols(
                 name TEXT PRIMARY KEY NOT NULL
             );
         """)
     if "config" not in table_names:
-        con.executescript("""
+        con.execute("""
             CREATE TABLE config(
                 key TEXT NOT NULL PRIMARY KEY,
                 value TEXT
@@ -70,15 +80,79 @@ def initialize_alphavantage_db():
                 ('total_requests')
             ;
         """)
+    if "rsi_5min" not in table_names:
+        con.execute("""
+            CREATE TABLE rsi_5min(
+                symbol TEXT NOT NULL,
+                timestamp INTEGER NOT NULL,
+                time_period INTEGER NOT NULL,
+                close REAL NOT NULL
+            );
+            CREATE UNIQUE INDEX rsi_5min_symbol_timestamp
+            ON rsi_5min(symbol, timestamp);
+        """);
+    con.commit()
+    con.close()
 
-initialize_alphavantage_db()
 
 def intraday_extended_slices():
     for year in range(1, 3):
         for month in range(1, 13):
             yield f"year{year}month{month}";
 
-def add_symbol_to_db(symbol):
+def get_alphavantage_error_code(req):
+    jayson = None
+    try:
+        jayson = req.json()
+    except:
+        pass
+    return jayson
+
+def update_symbols_in_db():
+    print('Updating')
+    con = alphavantage_db()
+    print(con.execute("SELECT name FROM symbols").fetchall())
+    symbol_names =  [symbol_name for (symbol_name,) in con.execute("SELECT name FROM symbols").fetchall()]
+    for symbol_name in symbol_names:
+        last_date_s = con.execute(f"""
+            SELECT timestamp FROM candlestick_5min
+            WHERE symbol = '{symbol_name}'
+            ORDER BY timestamp DESC
+            LIMIT 1;
+        """).fetchone()
+        last_datetime_stored = datetime.fromtimestamp(last_date_s[0], timezone.utc)
+        uri = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol_name}&interval=5min&apikey={alphavantage_key}&datatype=csv&outputsize=full'
+        req = alphavantage_request(uri)
+        rdr = csv.reader(req.content.decode('utf-8').splitlines(), delimiter=',')
+        last_time_entry = None
+        first_line = True
+        con.execute("BEGIN TRANSACTION;")
+        for (timestamp, open, high, low, close, volume) in rdr:
+            if first_line:
+                first_line = False
+                continue
+            # Alphavantage stores its timestamp in EST Since EST = UTC - 5
+            # then UTC = EST + 5
+            
+            con.execute(f"""
+                INSERT OR IGNORE INTO candlestick_5min (symbol, timestamp, open, high, low, close, volume)
+                VALUES ('{symbol_name}', strftime('%s','{timestamp}') + strftime('%%H', '5'), {open}, {high}, {low}, {close}, {volume});
+            """)
+            # TODO Use Instead
+            cur_datetime = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') + timedelta(hours=5)
+            cur_datetime = datetime.fromtimestamp(cur_datetime.timestamp(), timezone.utc)
+            if last_time_entry == None:
+                last_time_entry = cur_datetime
+            last_time_entry = min(last_time_entry, cur_datetime)
+        con.execute("COMMIT;")
+        print(last_time_entry)
+        if last_time_entry > last_datetime_stored:
+            add_symbol_to_db(symbol_name, last_datetime_stored)
+
+    con.commit()
+    con.close()
+
+def add_symbol_to_db(symbol, last_datetime_stored=None):
     if alphavantage_key == None:
         raise Exception()
     con = alphavantage_db()
@@ -86,23 +160,16 @@ def add_symbol_to_db(symbol):
     symbol_names = [symbol_name for (symbol_name,) in symbols]
     if symbol in symbol_names:
         print("WARNING", symbol, "is already in the database")
-        con.execute(f"INSERT INTO symbols (name) VALUES('{symbol}')")
 
+    con.execute(f"INSERT OR REPLACE INTO symbols (name) VALUES('{symbol}')")
     for slice_name in intraday_extended_slices():
         uri = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol={symbol}&interval=5min&slice={slice_name}&apikey={alphavantage_key}&adjusted=false"
-        time.sleep(0.2);
-        req = requests.get(uri)
-        jay = None
-        try:
-            jay = req.json()
-        except:
-            pass
-        if jay != None:
-            raise AlphvantageError(jay)
-
-        con.execute("BEGIN TRANSACTION;")
+        req = alphavantage_request(uri)
         first_row = True
-        rdr = csv.reader(req.content.decode('utf-8').splitlines(), delimiter=',');
+        last_time_entry = None
+        rdr = csv.reader(req.content.decode('utf-8').splitlines(), delimiter=',')
+        con.execute("BEGIN TRANSACTION;")
+        
         for (timestamp, open, high, low, close, volume) in rdr:
             if first_row:
                 first_row = False
@@ -114,9 +181,17 @@ def add_symbol_to_db(symbol):
                 INSERT OR IGNORE INTO candlestick_5min (symbol, timestamp, open, high, low, close, volume)
                 VALUES ('{symbol}', strftime('%s','{timestamp}') + strftime('%%H', '5'), {open}, {high}, {low}, {close}, {volume});
             """)
-            
-        con.execute("COMMIT;")
+            cur_datetime = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S') + timedelta(hours=5)
+            if last_time_entry == None:
+                last_time_entry = cur_datetime
+            last_time_entry = min(cur_datetime, last_time_entry)
+        
+        con.execute("END TRANSACTION;")
+        if last_datetime_stored != None and last_time_entry <= last_datetime_stored:
+            break
 
     con.commit()
     con.close()
 
+
+initialize_alphavantage_db()
