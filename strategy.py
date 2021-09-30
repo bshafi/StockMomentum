5c4dd09c4210665115a3ee61d645f4b8b2da5b15
Bilal Shafi <bshaf001@ucr.edu>
2021-09-30 16:33:41 -0700

    added vix strategy and a way to get returns from any strategy

---
+++ strategy.py
@@ -0,0 +1,80 @@
+from datetime import date, datetime, timezone
+from typing import Tuple,List
+from alphavantage import alphavantage_db
+from enum import Enum
+import sqlite3
+
+def iter_query(con: sqlite3.Connection, query, buf_size=100):
+    data = con.execute(query).fetchall()
+    for point in data:
+        yield point
+    #cur = con.execute(query)
+    #data = cur.fetchmany(buf_size)
+    #while len(data) != 0:
+    #    for i in range(len(data)):
+    #        yield data[i]
+    #    cur.fetchmany(buf_size)
+
+class StockAction(Enum):
+    BUY = 0
+    SELL = 1
+
+def buying_enclosed_vix_daily(sell_point, buy_point, start_date: datetime, end_date: datetime) -> List[Tuple[datetime, StockAction]]:
+    assert(0 <= sell_point <= 100)
+    assert(0 <= buy_point <= 100)
+    assert(buy_point < sell_point)
+
+    con = alphavantage_db()
+    QUERY = f"""SELECT timestamp, close as vix
+        FROM vix_daily
+        WHERE timestamp BETWEEN {start_date.timestamp()} AND {end_date.timestamp()}
+        ORDER BY timestamp ASC
+    """
+    actions = []
+    has_bought = False
+    bob = list(iter_query(con, QUERY))
+    for (timestamp, vix) in bob:
+        date = datetime.fromtimestamp(timestamp, timezone.utc)
+        if has_bought:
+            if vix < buy_point:
+                actions.append((date, StockAction.SELL))
+                has_bought = False
+        else:
+            if vix > sell_point:
+                actions.append((date, StockAction.BUY))
+                has_bought = True
+    return actions
+
+def percent_return_daily(con: sqlite3.Connection, h_actions: List[Tuple[datetime, StockAction]], symbol) -> float:
+    actions = sorted(h_actions, key=lambda x: x[0])
+    min_date = actions[0][0]
+    max_date = actions[-1][0]
+
+    data = con.execute(f"""
+        SELECT timestamp, close FROM candlestick_daily
+        WHERE symbol = '{symbol}' AND timestamp BETWEEN {min_date.timestamp()} AND {max_date.timestamp()}
+        ORDER BY timestamp ASC
+    """).fetchall()
+
+    gains = 0
+    last_buying_price = None
+    i = 0
+    for (timestamp, close) in data:
+        date = datetime.fromtimestamp(timestamp, timezone.utc)
+        if actions[i][0] <= date:
+            action_date, action = actions[i]
+            i = i + 1
+            if action == StockAction.BUY:
+                assert(last_buying_price == None)
+                last_buying_price = close
+            elif action == StockAction.SELL:
+                assert(last_buying_price != None)
+                gains = gains + (close - last_buying_price)
+                last_buying_price = None
+
+        if i >= len(actions):
+            break
+    return gains
+
+
+
