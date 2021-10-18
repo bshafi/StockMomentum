d534aa55d7710784c4bf50b5b2a8975e38107acf
Bilal Shafi <bshaf001@ucr.edu>
2021-10-18 12:37:42 -0700

    added aws lambda integration

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+from backtester import query_table, backtest_data
+import sm_util
+from sm_util import ArgumentError, historical_database
+
+sm_util.credentials_file = 'credentials/trader_bot0'
+
+def lambda_handler(event, context = None):
+    params = event['params']['querystring']
+    function = params['function']
+    
+    con = historical_database()
+    
+    try:
+        if function == 'QUERY':
+            return query_table(con, params)
+        elif function == 'BACKTEST':
+            return backtest_data(con, params)
+    except ArgumentError as arg_err:
+        return str.encode(arg_err.get_msg())
