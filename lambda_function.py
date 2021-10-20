a9760306e2f79b3fbce524e6b6d936274a3ce3f3
Bilal Shafi <bshaf001@ucr.edu>
2021-10-20 12:08:38 -0700

    small fixes

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+from time import localtime
 from backtester import query_table, backtest_data
 import sm_util
 from sm_util import ArgumentError, historical_database
@@ -17,3 +18,18 @@ def lambda_handler(event, context = None):
             return backtest_data(con, params)
     except ArgumentError as arg_err:
         return str.encode(arg_err.get_msg())
+
+print(lambda_handler({
+  "params": {
+    "querystring": {
+      "function": "BACKTEST",
+      "backtester": "sentiment_antivix",
+      "start_date": "2010-08-01",
+      "end_date": "2021-09-01",
+      "buy_point": "12",
+      "sell_point": "89",
+      "symbol": "LASR",
+      "observation_period": "10"
+    }
+  }
+}))
